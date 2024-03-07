35ddecdcfdf486148eeab78d6f157c8511bc71d7
meowmeow <nico55518@gmail.com>
2024-03-07 13:49:11 +0100

    added all kinds of curves in simplified weierstrass form. Changed Field to implement some traits

--- src/ecc/elliptic_curves.rs
+++ src/ecc/elliptic_curves.rs
@@ -1,6 +1,5 @@
 //General Elliptic Curves
 mod field;
-use std::clone;
 
 use field::Field;
 
@@ -11,52 +10,143 @@ struct Point<T>{
 
 
 
-struct EllipticCurveType1 <T: Field /* T::char() is assumed to not be 3 or 2*/> {
+struct EllipticCurveType1 <T: Field> {
     //E: y^2=x^3+ax+b
     d: T, // discriminant
     a: T, // 1st curve parameter
     b: T, // 2nd curve parameter
 }
 
-impl<T: Field> EllipticCurveType1 <T>{
+impl<T: Field> EllipticCurveType1 <T> {
     fn new(a:  T, b: T) -> Self {
+        if T::char() == 3 || T::char() == 2 {panic!("The field's characteristic must not be 2 or 3");}
         let mut x = a.pow(3);
         let mut y = b.pow(2);
-        x = T::scalar_left_multiplication(4,x);
-        y = T::scalar_left_multiplication(27, y);
+        x = T::scalar_left_multiplication(4,&x);
+        y = T::scalar_left_multiplication(27, &y);
         Self {
             a, 
             b,
-            d:  T::scalar_left_multiplication(-16, x.add(&y)),
+            d:  T::scalar_left_multiplication(-16, &(x+y)),
         }
     }
 
-    fn testPoint(self, p: Point<T>) -> bool{
+    fn test_point(self, p: Point<T>) -> bool{
         //tests if point satisfies equation
-        let ax = self.a.mul(&p.x);
-        let x3 = p.x.pow(3);
-        let z = ax.add(&x3).add(&self.b);
-        return T::equals(&p.y.pow(2), &z);
+        let ax = self.a * p.x;
+        let x3 = &p.x.pow(3);
+        let z = ax + *x3 + self.b;
+        p.y.pow(2) == z
     }
 }
 
-struct EllipticCurveType2NS<T: Field /*T::char() is assumed to be 2*/> {
+struct EllipticCurveType2NS<T: Field> {
     //E: y^2 + xy = x^3 + ax^2 + b
     a: T,
     b: T,
+    d: T,
     // in this case we always have d = b.
 }
 
 impl<T:Field> EllipticCurveType2NS<T>{
     fn new(a:T, b:T) -> Self {
+        if T::char() != 2 {panic!("The field's characteristic must be 2")}
         Self {
             a,
             b,
+            d: b,
         }
     }
 
-    fn testPoint(self, p: Point<T>) -> bool{
-        //todo
-        return true
+    fn test_point(self, p: Point<T>) -> bool {
+        let y2: T = p.y.pow(2);
+        let xy: T = p.x * p.y;
+        let lhs: T = y2 + xy;
+        let x3: T = p.x.pow(3);
+        let ax2: T = self.a * p.x.pow(2);
+        let rhs: T = x3 + ax2 + self.b;
+        lhs == rhs 
+    }
+}
+
+struct EllipticCurveType2S<T: Field> {
+    //y^2 + cy = x^3 + ax + b 
+    a: T,
+    b: T, 
+    c: T,
+    d: T,
+}
+
+impl<T:Field> EllipticCurveType2S<T> {
+    fn new(a: T, b: T, c: T) -> Self {
+        if T::char() != 2 {panic!("The field's characteristic must be 2")}
+        Self {
+            a,
+            b,
+            c,
+            d: c.pow(4),
+        }
+    }
+
+    fn test_point(self, p: Point<T>) -> bool {
+        let y2: T = p.y.pow(2);
+        let cy: T = p.y * self.c;
+        let lhs: T = y2 + cy;
+        let x3: T = p.x.pow(3);
+        let ax: T = self.a * p.x;
+        let rhs = x3 + ax + self.b;
+        lhs == rhs
+    }
+}
+
+struct EllipticCurveType3NS<T: Field> {
+    // y^2 = x^3 + ax^2 + b
+    a: T,
+    b: T,
+    d: T,
+}
+
+impl<T: Field> EllipticCurveType3NS<T> {
+    fn new(a: T, b: T) -> Self {
+        if T::char() != 3 {panic!("The field's characteristic must be 3")}
+        Self {
+            a,
+            b,
+            d: T::inv_mul(&(a.pow(3) * b)),
+        }
+    }
+
+    fn test_point(self, p: Point<T>) -> bool {
+        let lhs: T = p.y.pow(2);
+        let x3: T = p.x.pow(3);
+        let ax2: T = self.a * p.x.pow(2);
+        let rhs = x3 + ax2 + self.b;
+        lhs == rhs
+    }
+}
+
+struct EllipticCurveType3S<T: Field> {
+    // y^2 = x^3 + ax + b;
+    a: T,
+    b: T,
+    d: T,
+}
+
+impl<T: Field> EllipticCurveType3S<T> {
+    fn new(a: T, b: T) -> Self {
+        if T::char() != 3 {panic!("the field's characteristic must be 3")}
+        Self {
+            a,
+            b,
+            d: T::inv_mul(&a.pow(3)),
+        }
+    }
+
+    fn test_point(self, p: Point<T>) -> bool {
+        let lhs = p.y.pow(2);
+        let x3 = p.x.pow(3);
+        let ax = self.a * p.x;
+        let rhs = x3 + ax + self.b;
+        lhs == rhs
     }
 }
--- src/ecc/elliptic_curves/field.rs
+++ src/ecc/elliptic_curves/field.rs
@@ -1,12 +1,25 @@
-pub trait Field: {
+use std::ops;
+
+pub trait Field: Sized + ops::Add<Self, Output=Self> + ops::Mul<Self, Output=Self> + Eq + Copy{
     fn inv_mul(&self) -> Self;
     fn inv_add(&self) -> Self;
     fn neutral_mul() -> Self;
     fn neutral_add() -> Self;
-    fn pow(&self, b: i32) -> Self;
-    fn scalar_left_multiplication(a: i32, b: Self) -> Self;
-    fn add(&self, b: &Self) -> Self;
-    fn mul(&self, b: &Self) -> Self;
+    
+    fn pow(&self, b: i32) -> Self {
+        let mut x: Self = Self::neutral_mul();
+        for _i in 0..b.abs() {
+            x =  x * (*self)
+        }
+        if b >= 0 {x} else {Self::inv_mul(&self)} 
+    }
+
+    fn scalar_left_multiplication(a: i32, b: &Self) -> Self {
+        let mut x: Self = Self::neutral_add();
+        for _i in 0..a.abs() {
+            x = x + (*b);
+        }
+        if a >= 0 {x} else {Self::inv_add(&b)}
+    }
     fn char() -> u32;
-    fn equals(&self, b: &Self) -> bool;
 }
