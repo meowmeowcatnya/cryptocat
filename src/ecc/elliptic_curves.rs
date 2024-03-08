4bd54090aec82263739b8251614c3191a56fdb75
meowmeow <nico55518@gmail.com>
2024-03-08 18:32:37 +0100

    added addition rule for type 1 curves

--- src/ecc/elliptic_curves.rs
+++ src/ecc/elliptic_curves.rs
@@ -3,11 +3,11 @@ mod field;
 
 use field::Field;
 
-struct Point<T>{
-    x: T,
-    y: T
+#[derive(PartialEq)]
+enum Point<T: Field>{
+    Point{x: T,y: T},
+    PointAtInfinity
 }
-
 
 
 struct EllipticCurveType1 <T: Field> {
@@ -17,7 +17,7 @@ struct EllipticCurveType1 <T: Field> {
     b: T, // 2nd curve parameter
 }
 
-impl<T: Field> EllipticCurveType1 <T> {
+impl<T: Field + Eq> EllipticCurveType1 <T> {
     fn new(a:  T, b: T) -> Self {
         if T::char() == 3 || T::char() == 2 {panic!("The field's characteristic must not be 2 or 3");}
         let mut x = a.pow(3);
@@ -33,10 +33,66 @@ impl<T: Field> EllipticCurveType1 <T> {
 
     fn test_point(self, p: Point<T>) -> bool{
         //tests if point satisfies equation
-        let ax = self.a * p.x;
-        let x3 = &p.x.pow(3);
-        let z = ax + *x3 + self.b;
-        p.y.pow(2) == z
+        match p {
+            Point::PointAtInfinity => true, 
+            Point::Point { x, y} => {
+                let ax = self.a * x;
+                let x3 = x.pow(3);
+                let z = ax + x3 + self.b;
+                y.pow(2) == z
+
+            }
+        }
+    }
+
+    fn add_point(self, p1: Point<T>, p2: Point<T>) -> Point<T> {
+        //neutral rule
+        match p1 {
+            Point::PointAtInfinity => p2,
+            Point::Point { x, y } => {
+                let a = x;
+                let b = y;
+                match p2 {
+                    Point::PointAtInfinity => p1,
+                    Point::Point { x, y } => {
+                        //inverse rule 
+                        if a == x && b == T::inv_add(&y) {Point::PointAtInfinity} else {
+                                // double rule
+                            if p1 == p2 {
+                                // x coordinate
+                                let x2 = x.pow(2);
+                                let num = T::scalar_left_multiplication(3, &x2) + self.a;
+                                let den = T::scalar_left_multiplication(2, &y);
+                                let frac = num * T::inv_mul(&den);
+                                let x3 = frac.pow(2) + T::inv_add(&T::scalar_left_multiplication(2, &x));
+
+                                //y coordinate
+                                let diff = x + T::inv_add(&x3);
+                                let prod = frac * diff;
+                                let y3 = prod + T::inv_add(&y);
+
+                                Point::Point { x: x3, y: y3 }
+                            } else {
+                                //addition rule
+                                // x coordinate 
+                                let num = y + T::inv_add(&b);
+                                let den = x + T::inv_add(&a);
+                                let frac = num * T::inv_mul(&den);
+                                let frac2 = frac.pow(2);
+                                let x3 = frac2 + T::inv_add(&a) + T::inv_add(&x);  
+
+                                // y coordinate
+                                let diff = a + T::inv_add(&x3);
+                                let prod = frac * diff;
+                                let y3 = prod + T::inv_add(&b);
+                                
+                                Point::Point { x: x3, y: y3}
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
 
@@ -59,13 +115,18 @@ impl<T:Field> EllipticCurveType2NS<T>{
     }
 
     fn test_point(self, p: Point<T>) -> bool {
-        let y2: T = p.y.pow(2);
-        let xy: T = p.x * p.y;
-        let lhs: T = y2 + xy;
-        let x3: T = p.x.pow(3);
-        let ax2: T = self.a * p.x.pow(2);
-        let rhs: T = x3 + ax2 + self.b;
-        lhs == rhs 
+        match p {
+            Point::PointAtInfinity => true,
+            Point::Point { x, y } => {
+                let y2: T = y.pow(2);
+                let xy: T = x * y;
+                let lhs: T = y2 + xy;
+                let x3: T = x.pow(3);
+                let ax2: T = self.a * x.pow(2);
+                let rhs: T = x3 + ax2 + self.b;
+                lhs == rhs 
+            }
+        }
     }
 }
 
@@ -89,13 +150,18 @@ impl<T:Field> EllipticCurveType2S<T> {
     }
 
     fn test_point(self, p: Point<T>) -> bool {
-        let y2: T = p.y.pow(2);
-        let cy: T = p.y * self.c;
-        let lhs: T = y2 + cy;
-        let x3: T = p.x.pow(3);
-        let ax: T = self.a * p.x;
-        let rhs = x3 + ax + self.b;
-        lhs == rhs
+        match p {
+            Point::PointAtInfinity => true, 
+            Point::Point { x, y } => {
+            let y2: T = y.pow(2);
+            let cy: T = y * self.c;
+            let lhs: T = y2 + cy;
+            let x3: T = x.pow(3);
+            let ax: T = self.a * x;
+            let rhs = x3 + ax + self.b;
+            lhs == rhs
+            }
+        }
     }
 }
 
@@ -117,11 +183,16 @@ impl<T: Field> EllipticCurveType3NS<T> {
     }
 
     fn test_point(self, p: Point<T>) -> bool {
-        let lhs: T = p.y.pow(2);
-        let x3: T = p.x.pow(3);
-        let ax2: T = self.a * p.x.pow(2);
-        let rhs = x3 + ax2 + self.b;
-        lhs == rhs
+        match p {
+            Point::PointAtInfinity => true,
+            Point::Point { x, y } => {
+            let lhs: T = y.pow(2);
+            let x3: T = x.pow(3);
+            let ax2: T = self.a * x.pow(2);
+            let rhs = x3 + ax2 + self.b;
+            lhs == rhs
+            }
+        }
     }
 }
 
@@ -143,10 +214,15 @@ impl<T: Field> EllipticCurveType3S<T> {
     }
 
     fn test_point(self, p: Point<T>) -> bool {
-        let lhs = p.y.pow(2);
-        let x3 = p.x.pow(3);
-        let ax = self.a * p.x;
-        let rhs = x3 + ax + self.b;
-        lhs == rhs
+        match p {
+            Point::PointAtInfinity => true, 
+            Point::Point { x, y } => {
+            let lhs = y.pow(2);
+            let x3 = x.pow(3);
+            let ax = self.a * x;
+            let rhs = x3 + ax + self.b;
+            lhs == rhs
+            }
+        }
     }
 }
